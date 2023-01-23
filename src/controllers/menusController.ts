68a5ccc8477b9489fd9f0e0431553ffc7b394282
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-23 20:40:53 +0100

    feat : method MenusController.getAll

--- src/controllers/menusController.ts
+++ src/controllers/menusController.ts
@@ -1,28 +1,27 @@
-import express,{Request,Response} from 'express';
-import { MenuServices } from 'src/services/menuServices';
+import { Request, Response } from 'express';
+import { MenuServices } from '../services/menuServices';
 import { Menu } from '../entities/menu';
-import { AppDataSource } from '../module/clientData';
+import { Responser } from '../module/Responser';
+import { faillingId, faillingPrice, faillingString } from '../module/faillingTest';
 
 const menuServices = new MenuServices()
 
 export class MenusController {
     async getAll(req: Request, res: Response) {
-        menuServices.getAll() ;
-    }
-    async getById(req: Request, res: Response) {
-        menuServices.getById() ;
-    }
 
-    async new(req: Request, res: Response) {
-        menuServices.new() ;
-        
-    }
-    async edit(req: Request, res: Response) {
-        menuServices.edit() ;
-        
-    }
-    async delete(req: Request, res: Response) {
-        menuServices.delete() ;
-        
+        const responser = new Responser<Menu[]>(req, res);
+
+        try {
+            const data = await menuServices.getAll();
+
+            responser.status = 200;
+            responser.message = `Récupération de tous les menus`;
+            responser.data = data;
+            responser.send();
+        }
+        catch (err: any) {
+            console.log(err.stack)
+            responser.send();
+        }
     }
 }
