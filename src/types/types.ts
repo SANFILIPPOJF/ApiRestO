d23387353e9f9730ff5fb5af328524a6ccd1b007
SANFILIPPOJF <jean-francois.sanfilippo@sncf.fr>
2023-01-24 13:49:46 +0100

    feat : type userPartial, userWithToken

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export type TDataToken = {
+    id: number,
+    admin_lvl: number,
+    name: string,
+    token: string
+}
+
+export type TDataPartialUser = {
+    id: number,
+    name: string,
+    admin_lvl: number
+};
