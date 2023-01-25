b984bcb0c3da687e763d6ab3931db93b93957b08
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-25 14:51:05 +0100

    fix : correctif de typage

--- src/entities/order.ts
+++ src/entities/order.ts
@@ -3,23 +3,23 @@ import { OrderLine } from "./orderLine"
 import { Resto } from "./resto"
 import { Users } from "./user"
 
-@Entity()
+@Entity('orders')
 export class Order extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number
 
-    @Column()
+    @Column({default : new Date(),nullable : false})
     created_at: Date
 
-    @Column()
-    served_at: Date
+    @Column({ type: 'date', nullable : true })
+    served_at: Date | null
 
     @ManyToOne(() => Users, (user) => user.id)
-    user_id: Users
+    user_id: number
 
     @ManyToOne(() => Resto, (resto) => resto.id)
-    resto_id: Resto
+    resto_id: number
     
     @OneToMany(() => OrderLine, (line) => line.order_id)
-    lines: OrderLine[]
+    lines: number[]
 }
