579c4832f80c015c2529c47e98fcf4ab2632f737
SANFILIPPOJF <jean-francois.sanfilippo@sncf.fr>
2023-01-26 10:31:12 +0100

    feat : lines controller router, service

--- src/routes/linesRouter.ts
+++ src/routes/linesRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-//import { LinesController } from '../controllers/linesController';
+import { LinesController } from '../controllers/linesController';
 
 export const linesRouter = express.Router();
 
-//const linesController = new LinesController();
-
-/* usersRouter.post('/register', usersController.register)
-usersRouter.post('/login', usersController.login) */
+const linesController = new LinesController();
+linesRouter.get('/:id', linesController.getFromOrder)
+linesRouter.post('/', linesController.new)
+linesRouter.put('/', linesController.edit)
+linesRouter.delete('/', linesController.delete)
